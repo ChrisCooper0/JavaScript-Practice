88254235f483bdc7e271f3aff9077babe727ddce
Chris Cooper <chriscooper@Chriss-MacBook-Air.local>
2021-04-13 12:09:25 +0100

    truthy falsey

---
+++ truthyfalsey.js
@@ -0,0 +1,5 @@
+// FALSEY:
+// false, 0, -0, "" (an empty string), NaN, undefined, and null
+
+// Everything else in JavaScript is TRUTHY
+// true, 1, -1, "string", Object {}, Array [], Infinity, -Infinity
