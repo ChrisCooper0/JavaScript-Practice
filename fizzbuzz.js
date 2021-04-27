56d40cd0b564c74998d0318e7b51ea398ff04194
Chris Cooper <chriscooper@Chriss-MacBook-Air.local>
2021-04-27 10:43:35 +0100

    added solution

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,3 +1,4 @@
+// Solution looping over a range of numbers
 for (let i = 1; i <= 100; i++) {
   if (i % 15 === 0) {
     console.log(i + " FizzBuzz");
@@ -9,3 +10,18 @@ for (let i = 1; i <= 100; i++) {
     console.log(i);
   }
 }
+
+// Solution where a function takes in one number
+fizzBuzz = (num) => {
+  let result = "";
+  if ((num % 3 === 0) & (num % 5 === 0)) {
+    return (result += "FizzBuzz");
+  } else if (num % 5 === 0) {
+    return (result += "Buzz");
+  } else if (num % 3 === 0) {
+    return (result += "Fizz");
+  } else {
+    return result;
+  }
+};
+console.log(fizzBuzz(15)); // Prints 'FizzBuzz'
