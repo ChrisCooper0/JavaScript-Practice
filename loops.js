bbce8c996655e7e925ec23908f907e7c42ee18d1
ChrisCooper0 <christopher.cooper@mail.com>
2021-05-12 17:52:52 +0100

    add exercises

--- loops.js
+++ loops.js
@@ -165,3 +165,70 @@ evens(8);
 //  2
 //  4
 //  6
+
+// Write a function named `fiveMultiplesOf` that accepts a number as an argument. The function should
+// print out the first five multiples of the given number. The function doesn't need to return any
+// value. It should just print to the terminal.
+
+let fiveMultiplesOf = (num) => {
+  for (let i = 1; i <= 5; i++) {
+    console.log(num * i);
+  }
+};
+
+fiveMultiplesOf(7);
+// prints
+//  7
+//  14
+//  21
+//  28
+//  35
+
+fiveMultiplesOf(3);
+// prints
+//  3
+//  6
+//  9
+//  12
+//  15
+
+// Write a function named `sumUpTo(max)` that accepts a max number as an argument. The function should
+// return the total sum of all whole numbers from 1 to the max, inclusive.
+//
+// For example, sumUpTo(4) should return 10 because 1 + 2 + 3 + 4 = 10.
+
+let sumUpTo = (max) => {
+  let sum = 0;
+  for (let i = 1; i <= max; i++) {
+    sum += i;
+  }
+  return sum;
+};
+console.log(sumUpTo(4)); // 10
+console.log(sumUpTo(5)); // 15
+console.log(sumUpTo(2)); // 3
+
+// Write a function named `noOhs` that accepts a string as an argument. The functions should print the
+// characters of the string one by one except the character 'o'. The function doesn't need to return
+// any value. It should just print to the terminal.
+
+let noOhs = (str) => {
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] !== "o") {
+      console.log(str[i]);
+    }
+  }
+};
+
+noOhs("code");
+// prints
+//  c
+//  d
+//  e
+
+noOhs("school");
+// prints
+//  s
+//  c
+//  h
+//  l
