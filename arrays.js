48a6d328cb5196d2fa89ac8576df308fa3d3c6be
ChrisCooper0 <christopher.cooper@mail.com>
2021-05-17 21:24:09 +0100

    add example

--- arrays.js
+++ arrays.js
@@ -188,3 +188,21 @@ let divisors = (num) => {
 console.log(divisors(15)); // [1, 3, 5, 15]
 console.log(divisors(7)); // [1, 7]
 console.log(divisors(24)); // [1, 2, 3, 4, 6, 8, 12, 24]
+
+// Write a function `makeAcronym` that accepts a sentence string as an argument. The function should
+// return a string containing the first character of each word in the sentence.
+
+let makeAcronym = (str) => {
+  let words = str.split(" ");
+  let acronym = "";
+  for (let i = 0; i < words.length; i++) {
+    let word = words[i];
+    acronym += word[0];
+  }
+  return acronym.toUpperCase();
+};
+
+console.log(makeAcronym("New York")); // NY
+console.log(makeAcronym("same stuff different day")); // SSDD
+console.log(makeAcronym("Laugh out loud")); // LOL
+console.log(makeAcronym("don't over think stuff")); // DOTS
