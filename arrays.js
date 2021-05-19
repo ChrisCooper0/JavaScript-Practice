2a4b2814f523c168d87fb48dd84b4db7f4e7a0c2
ChrisCooper0 <christopher.cooper@mail.com>
2021-05-19 07:10:29 +0100

    add exercises

--- arrays.js
+++ arrays.js
@@ -305,3 +305,25 @@ let wordCount = (sentence, targetWords) => {
 console.log(wordCount("open the window please", ["please", "open", "sorry"])); // 2
 console.log(wordCount("drive to the cinema", ["the", "driver"])); // 1
 console.log(wordCount("can I have that can", ["can", "I"])); // 3
+
+// Write a function `lengthiestWord` that accepts a sentence string as an argument. The function should
+// return the longest word of the sentence. If there is a tie, return the word that appears later
+// in the sentence.
+
+let lengthiestWord = (str) => {
+  let words = str.split(" ");
+  let longestWord = words[0];
+
+  for (let i = 1; i < words.length; i++) {
+    if (words[i].length >= longestWord.length) {
+      longestWord = words[i];
+    }
+  }
+  return longestWord;
+};
+
+console.log(lengthiestWord("I am pretty hungry")); // 'hungry'
+console.log(lengthiestWord("we should think outside of the box")); // 'outside'
+console.log(lengthiestWord("down the rabbit hole")); // 'rabbit'
+console.log(lengthiestWord("simmer down")); // 'simmer'
+
