384a50b15bdfcd72e77c519a99eb05f6e3649ee9
ChrisCooper0 <christopher.cooper@mail.com>
2021-06-02 08:36:57 +0100

    add exercise

--- nestedarrays.js
+++ nestedarrays.js
@@ -35,3 +35,38 @@ print2d(array2);
 //  11
 //  42
 //  100
+
+// Write a function `makeMatrix(m, n, value)` that accepts three arguments. The function should return
+// a 2-dimensional array of height `m` and width `n` that contains the `value` as every element.
+
+let makeMatrix = (m, n, value) => {
+  let matrix = [];
+  for (let i = 0; i < m; i++) {
+    matrix.push([]);
+    for (let j = 0; j < n; j++) {
+      matrix[i].push(value);
+    }
+  }
+  return matrix;
+};
+
+console.log(makeMatrix(3, 5, null));
+// [
+//   [ null, null, null, null, null ],
+//   [ null, null, null, null, null ],
+//   [ null, null, null, null, null ]
+// ]
+
+console.log(makeMatrix(4, 2, "x"));
+// [
+//   [ 'x', 'x' ],
+//   [ 'x', 'x' ],
+//   [ 'x', 'x' ],
+//   [ 'x', 'x' ]
+// ]
+
+console.log(makeMatrix(2, 2, 0));
+// [
+//   [ 0, 0 ],
+//   [ 0, 0 ]
+// ]
