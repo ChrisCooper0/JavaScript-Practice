27a84473db0202edb6cd59b3e8012cee491cdb53
ChrisCooper0 <christopher.cooper@mail.com>
2021-06-02 19:54:08 +0100

    add exercises

--- nestedarrays.js
+++ nestedarrays.js
@@ -47,6 +47,7 @@ let makeMatrix = (m, n, value) => {
       matrix[i].push(value);
     }
   }
+
   return matrix;
 };
 
@@ -69,4 +70,84 @@ console.log(makeMatrix(2, 2, 0));
 // [
 //   [ 0, 0 ],
 //   [ 0, 0 ]
+// ]
+
+// Write a function `totalProduct(array)` that accepts a 2D array of numbers. The function should return
+// the total product of all numbers in the array.
+
+let totalProduct = (array) => {
+  let total = 1;
+
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array[i].length; j++) {
+      total *= array[i][j];
+    }
+  }
+  return total;
+};
+
+let array3 = [
+  [3, 5, 2],
+  [6, 2],
+];
+console.log(totalProduct(array3)); // 360
+
+let array4 = [
+  [4, 6],
+  [2, 3],
+  [1, 2],
+];
+console.log(totalProduct(array4)); // 288
+
+// Write a function `twoSumPairs(numbers, target)` that accepts an array of numbers and a target number
+// as arguments. The function should return a 2D array containing all unique pairs of elements that
+// sum to the target.
+
+let twoSumPairs = (numbers, target) => {
+  let pairs = [];
+  for (let i = 0; i < numbers.length; i++) {
+    for (let j = i + 1; j < numbers.length; j++) {
+      if (numbers[i] + numbers[j] === target) {
+        pairs.push([numbers[i], numbers[j]]);
+      }
+    }
+  }
+  return pairs;
+};
+
+console.log(twoSumPairs([2, 3, 4, 6, 5], 8)); // [ [2, 6], [3, 5] ]
+console.log(twoSumPairs([10, 7, 4, 5, 2], 12)); // [ [10, 2], [7, 5] ]
+console.log(twoSumPairs([3, 9, 8], 11)); // [ [3, 8] ]
+console.log(twoSumPairs([3, 9, 8], 10)); // [ ]
+
+// Write a function `zipper` that accepts two arrays as arguments. The function should return a 2D
+// array containing pairs of elements at the same indices. You can assume that the arrays have the
+// same length.
+
+let zipper = (arr1, arr2) => {
+  let pairs = [];
+  for (let i = 0; i < arr1.length; i++) {
+    let pair = [arr1[i], arr2[i]];
+    pairs.push(pair);
+  }
+  return pairs;
+};
+
+let array5 = ["a", "b", "c", "d"];
+let array6 = [-1, -2, -3, -4];
+console.log(zipper(array5, array6));
+// [
+//   ['a', -1],
+//   ['b', -2],
+//   ['c', -3],
+//   ['d', -4],
+// ]
+
+let array7 = ["whisper", "talk", "shout"];
+let array8 = ["quiet", "normal", "loud"];
+console.log(zipper(array7, array8));
+// [
+//   ['whisper', 'quiet'],
+//   ['talk', 'normal'],
+//   ['shout', 'loud'],
 // ]
