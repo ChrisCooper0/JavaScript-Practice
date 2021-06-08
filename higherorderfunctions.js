3e003cfd78cdc1fe84440b2b3a439eb5060dbb86
ChrisCooper0 <christopher.cooper@mail.com>
2021-06-08 20:14:49 +0100

    add example

--- higherorderfunctions.js
+++ higherorderfunctions.js
@@ -67,3 +67,29 @@ let isOld = function (person) {
 let result4 = doggos.some(isOld);
 
 console.log(result4); // false
+
+// snippet-5
+
+let nums = [3, 7, 2, 9];
+
+let res1 = nums.reduce(function (a, b) {
+  return a + b;
+});
+
+console.log(res1); // 21
+
+let res2 = nums.reduce(function (a, b) {
+  return a + b;
+}, -10);
+
+console.log(res2); // 11
+
+let res3 = nums.reduce(function (a, b) {
+  if (b < a) {
+    return b;
+  } else {
+    return a;
+  }
+});
+
+console.log(res3); // 2
