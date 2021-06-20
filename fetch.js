61b0666134f227e61f5cdff20a380bde9ed9aee9
ChrisCooper0 <christopher.cooper@mail.com>
2021-06-20 11:01:02 +0100

    added examples

--- fetch.js
+++ fetch.js
@@ -1,9 +1,22 @@
-// Fetch
+// Fetch API -  Browser API for HTTP (AJAX) Requests
+// Default - GET Requests, supports other methods as well
+// Returns Promise
 
+const url = "https://jsonplaceholder.typicode.com/users";
+
+// Basic Fetch request
+// fetch(url)
+//   .then((response) => response.json())
+//   .then((data) => console.log(data))
+//   .catch((err) => console.log(err));
+
+// console.log(fetch(url));
+
+// Fetch request via function call
 const apiCall = () => {
   const result = document.getElementById("result");
 
-  fetch("https://jsonplaceholder.typicode.com/users")
+  fetch(url)
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
