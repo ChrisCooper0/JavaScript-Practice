8732f13cc317a78f89b4e8cb13076f2ac0986ee1
ChrisCooper0 <christopher.cooper@mail.com>
2021-07-10 18:26:11 +0100

    added katas

---
+++ katas.js
@@ -0,0 +1,20 @@
+// A collection of katas with corresponding tests
+
+// Original solution
+// const disemvowel = (str) => {
+//   let newStr = "";
+//   const vowels = "aeiouAEIOU";
+//   for (let i = 0; i < str.length; i++) {
+//     if (!vowels.includes(str[i])) {
+//       newStr += str[i];
+//     }
+//   }
+//   return newStr;
+// };
+
+// Refactored solution
+const disemvowel = (str) => {
+  return str.replace(/[aeiou]/gi, "");
+};
+
+console.log(disemvowel("TestAEIOUaeiouString"));
